7d4c120b6e1988577c4e885154d8facb4ffb33e1
Diegox <diego.adrian.777@gmail.com>
2025-11-20 03:01:59 -0400

    Implement hasVoted

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -3,7 +3,11 @@
 import { Answer, Question, Vote } from "@/database";
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { CreateVoteSchema, UpdateVoteCountSchema } from "../validations";
+import {
+  CreateVoteSchema,
+  HasVotedSchema,
+  UpdateVoteCountSchema,
+} from "../validations";
 import mongoose, { ClientSession } from "mongoose";
 
 export async function updateVoteCount(
@@ -100,6 +104,46 @@ export async function createVote(
     return { success: true };
   } catch (error) {
     await session.abortTransaction();
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function hasVoted(
+  params: HasVotedParams
+): Promise<ActionResponse<HasVotedResponse>> {
+  const validationResult = await action({
+    params,
+    schema: HasVotedSchema,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { targetId, targetType } = validationResult.params!;
+  const userId = validationResult.session?.user?.id;
+
+  try {
+    const vote = await Vote.findOne({
+      author: userId,
+      actionId: targetId,
+      actionType: targetType,
+    });
+    if (!vote) {
+      return {
+        success: false,
+        data: { hasUpvoted: false, hasDownvoted: false },
+      };
+    }
+    return {
+      success: true,
+      data: {
+        hasUpvoted: vote.voteType === "upvote",
+        hasDownvoted: vote.voteType === "downvote",
+      },
+    };
+  } catch (error) {
     return handleError(error) as ErrorResponse;
   }
 }
